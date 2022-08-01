cc80e13d7295f6535f96d7d33561cdb6d2834006
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2022-08-01 13:15:32 +0100

    feat: merge updates from ne04j library

---
+++ packages/graphql-toolbox/jest.config.js
@@ -0,0 +1,14 @@
+const globalConf = require("../../jest.config.base");
+
+module.exports = {
+    ...globalConf,
+    testEnvironment: "jsdom",
+    displayName: "@neo4j/graphql-toolbox",
+    roots: ["<rootDir>/packages/graphql-toolbox/src/", "<rootDir>/packages/graphql-toolbox/tests/"],
+    coverageDirectory: "<rootDir>/packages/graphql-toolbox/coverage/",
+    globals: {
+        "ts-jest": {
+            tsconfig: "<rootDir>/packages/graphql-toolbox/tsconfig.test.json",
+        },
+    },
+};
